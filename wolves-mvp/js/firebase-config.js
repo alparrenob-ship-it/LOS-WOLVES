e095b66e2ebaffb2602b260d391a8d25f9d885d9
alparrenob-ship-it <alparrenob@eightacademy.edu.ec>
2026-06-16 22:17:14 -0500

    Correct Firebase API key exactly from console

--- wolves-mvp/js/firebase-config.js
+++ wolves-mvp/js/firebase-config.js
@@ -1,5 +1,5 @@
 window.WOLVES_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCzN1iIJX04B1nPMgFsymEQ5_-2eYou4",
+  apiKey: "AIzaSyCzNi1jIlJX04B1nPMgFsymEQ5_-2eYou4",
   authDomain: "wolves-mvp-eight.firebaseapp.com",
   projectId: "wolves-mvp-eight",
   storageBucket: "wolves-mvp-eight.firebasestorage.app",
