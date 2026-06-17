09eb915c2f071c12826b428972136e70a11f2248
alparrenob-ship-it <alparrenob@eightacademy.edu.ec>
2026-06-16 21:39:49 -0500

    Add Firebase project configuration

---
+++ wolves-mvp/js/firebase-config.js
@@ -0,0 +1,8 @@
+window.WOLVES_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCzN1iILJX04B1nPMgFsymE05_2eYou4",
+  authDomain: "wolves-mvp-eight.firebaseapp.com",
+  projectId: "wolves-mvp-eight",
+  storageBucket: "wolves-mvp-eight.firebasestorage.app",
+  messagingSenderId: "373116842085",
+  appId: "1:373116842085:web:30873ef1da1712a5eb568c"
+};
